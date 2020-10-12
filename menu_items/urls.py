ef6ff0c123526be074c7ac28f63cdc0e8e823fe4
Caleb Nash <calebnash@Calebs-MacBook-Pro.local>
2020-10-12 15:06:28 -0400

    menu_items will save

---
+++ menu_items/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+
+from .views import MenuItemListView
+
+
+urlpatterns = [
+    path('',MenuItemListView.as_view(), name='home'),
+]
--- menu_items/views.py
+++ menu_items/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+
+from .models import MenuItem
+
+
+
+
+class MenuItemListView(ListView):
+    model = MenuItem
